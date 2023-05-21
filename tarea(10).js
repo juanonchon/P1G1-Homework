81543a99f186a7f2ee0115907bf98df76c824518
juanonchon <132412187+juanonchon@users.noreply.github.com>
2023-05-21 18:07:39 -0400

    Add files via upload

---
+++ tarea(10).js
@@ -0,0 +1,17 @@
+
+function mayorAByC(){
+    var A = parseIntprompt("ingrese A", 10)
+    var B = parseIntprompt("ingrese B", 7)
+    var C = parseIntprompt("ingrese C", 3)
+    
+    if ((A > B) && (C)) {
+            alert("el mayor es A:" + A)
+        }else{
+            alert("el mayor es B:" + B)
+            if (0 > C) {
+        }else{
+            alert("el mayor es C:" + C)
+        }
+    }
+}
+
